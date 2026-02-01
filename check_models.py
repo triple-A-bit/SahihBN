b82b76ba5e3e72806b5e2fbcb1b873d196103d6f
triple-A-bit <akmalarif2011@gmail.com>
2026-02-01 16:41:04 +0800

    Update check_models.py

--- check_models.py
+++ check_models.py
@@ -2,7 +2,7 @@ import google.generativeai as genai
 import os
 
 # Put your API KEY here directly just to test
-API_KEY = "PASTE_YOUR_API_KEY_HERE"
+API_KEY = "AIzaSyBAQgH9k4xw8ZY5vC7Jiut4XGcwh5JMSdg"
 genai.configure(api_key=API_KEY)
 
 print("List of available models for you:")
